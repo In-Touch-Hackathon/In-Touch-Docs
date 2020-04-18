d1768ca10ef99d6e5f8b45e3523700bbe1bd6e41
Cheng-Zhen Yang <chengzhenyang@gmail.com>
2020-04-18 19:21:22 +1200

    Updates

---
+++ precache-manifest.5c8ba60907bcab37aca5fd121f22fc39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4065cdb58d9954a0649d1221315fa34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15a5151239f35ea0a50c",
+    "url": "/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "ea9b70a14b40e1fa0f1d",
+    "url": "/static/css/main.67afbd9b.chunk.css"
+  },
+  {
+    "revision": "15a5151239f35ea0a50c",
+    "url": "/static/js/2.3271330a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3271330a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9b70a14b40e1fa0f1d",
+    "url": "/static/js/main.7ae40334.chunk.js"
+  },
+  {
+    "revision": "e2437be2f72929314631",
+    "url": "/static/js/runtime-main.654ef462.js"
+  }
+]);
